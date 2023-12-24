f9d67cdbff0b9db2b4a16a0ceb42d78899f0d6c7
JhomeWu <qu4qu426com123@gmail.com>
2023-12-24 22:49:54 +0800

    Feat: Create base data structure

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(),
+            'image_url' => fake()->imageUrl(),
+            'content' => fake()->paragraphs(3, true),
+            'is_active' => fake()->boolean(),
+            'user_id' => fn () => User::factory()->create()->id,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,12 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $testUserId = \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+        \App\Models\Post::factory(10)->create([
+            'user_id' => $testUserId,
+        ]);
     }
 }
